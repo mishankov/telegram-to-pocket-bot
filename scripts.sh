5b8b78be7f4d906148832519da71896d86cae7ee
Denis Mishankov <mishankov@mail.com>
2022-11-22 21:25:01 +0300

    add -e (use_env_file) param to script

--- scripts.sh
+++ scripts.sh
@@ -3,6 +3,13 @@
 command="$1"
 echo "Executing command ${command}"
 
+use_env_file="$2"
+if [[ ${use_env_file} == "-e" ]]; then
+    echo "Loading variables from .env file"
+    export $(grep -v '^#' .env | xargs -d '\n')
+fi
+
+
 if [[ ${command} == "build" ]]; then
     nim c -d:ssl -d:useOpenssl3 -d:release -f:on -o:build/tg-to-pocket src/main
     nim c -d:ssl -d:useOpenssl3 -d:release -f:on -o:build/generate_token src/generate_token
@@ -17,20 +24,16 @@ elif [[ ${command} == "install" ]]; then
 
 
 elif [[ ${command} == "run_build" ]]; then
-    export $(grep -v '^#' .env | xargs -d '\n')
     build/tg-to-pocket
 
 elif [[ ${command} == "run" ]]; then
-    export $(grep -v '^#' .env | xargs -d '\n')
     nim c -d:ssl -d:useOpenssl3 -f:on -o:build/tg-to-pocket -r src/main
 
 
 elif [[ ${command} == "run_generate_build" ]]; then
-    export $(grep -v '^#' .env | xargs -d '\n')
     build/generate_token
 
 elif [[ ${command} == "run_generate" ]]; then
-    export $(grep -v '^#' .env | xargs -d '\n')
     nim c -d:ssl -d:useOpenssl3 -f:on -o:build/generate_token -r src/generate_token
 
 
@@ -42,6 +45,8 @@ else
     echo "  install             - install dependencies"
     echo "  run_build           - run optimized build"
     echo "  run                 - run for development"
-    echo "  run_generate_build  - run for development"
+    echo "  run_generate_build  - run generate token optimized build"
     echo "  run_generate        - run generate token for development"
+
+    exit 1
 fi
